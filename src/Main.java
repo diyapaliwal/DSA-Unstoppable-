489aeb2b07baf9ff8a2f74e9e4012f5437382b61
Diya Paliwal <115967817+diyapaliwal@users.noreply.github.com>
2025-06-02 12:54:00 +0530

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -64,17 +64,3 @@
 //        }
 //    }
 //}
-
-import java.util.Scanner;
-public class Main{
-    public static void main(String[] args){
-        Scanner sc= new Scanner(System.in);
-        System.out.println("range of number: ");
-        int num = sc.nextInt();
-        int j= 0;
-        for(int i=0; i<=num;i++){
-            j=j+i;
-        }
-        System.out.println("answer is: "+ j);
-    }
-}
